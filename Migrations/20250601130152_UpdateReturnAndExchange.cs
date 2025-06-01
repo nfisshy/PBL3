c018d888971b74331cdcc0618f20efbccc26a798
nfisshy <vdatmain17@gmail.com>
2025-06-01 20:02:56 +0700

    add exchangeandreturn

---
+++ Migrations/20250601130152_UpdateReturnAndExchange.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PBL_3.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateReturnAndExchange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReturnExchanges",
+                columns: table => new
+                {
+                    ReturnExchangeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    RequestDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ResponseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReturnExchanges", x => x.ReturnExchangeId);
+                    table.ForeignKey(
+                        name: "FK_ReturnExchanges_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ReturnExchanges_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReturnExchanges_OrderId",
+                table: "ReturnExchanges",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReturnExchanges_ProductId",
+                table: "ReturnExchanges",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReturnExchanges");
+        }
+    }
+}
---
+++ Models/Enums/ExchangeStatus.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PBL3.Enums
+{
+    public enum ExchangeStatus
+    {
+        WaitConfirm,
+        Rejected,
+        Approved,
+    }
+}
