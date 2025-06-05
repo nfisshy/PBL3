c7e46d9936a85c018bf015e07784503a10409230
dusk5archiy <dusqdn.eml@gmail.com>
2025-06-05 20:14:38 +0700

    by_thanh

--- DTO/Admin/Admin_OrderManagementDTO.cs
+++ DTO/Admin/Admin_OrderManagementDTO.cs
@@ -3,19 +3,17 @@ using System.ComponentModel.DataAnnotations;
 using PBL3.Enums;
 
 namespace PBL3.DTO.Admin
-{
-    public class Admin_OrderManagementDTO
+{    public class Admin_OrderManagementDTO
     {
-        public int Id { get; set; }
-        public string OrderCode { get; set; }
-        public string BuyerName { get; set; }
-        public string SellerName { get; set; }
-        public decimal TotalAmount { get; set; }
-        public DateTime OrderDate { get; set; }
-        public bool PaymentStatus { get; set; }
-        public OrdStatus OrderStatus { get; set; }
-        public PayMethod PaymentMethod { get; set; }
-        public string Address { get; set; }
-        public int QuantityTypeOfProduct { get; set; }
+        public required int Id { get; set; }
+        public required string BuyerName { get; set; }
+        public required string SellerName { get; set; }
+        public required decimal TotalAmount { get; set; }
+        public required DateTime OrderDate { get; set; }
+        public required bool PaymentStatus { get; set; }
+        public required OrdStatus OrderStatus { get; set; }
+        public required PayMethod PaymentMethod { get; set; }
+        public required string Address { get; set; }
+        public required int QuantityTypeOfProduct { get; set; }
     }
 }
